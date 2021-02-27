6420eb2dd0e7ac5820ee76f916f627c30bd6b810
mimmackk <kjmimmack@gmail.com>
2021-02-27 15:35:56 -0600

    Optimized JavaScript version of unit ball function

--- live.js
+++ live.js
@@ -1,137 +1,63 @@
-// Frankenstein conversion of the python plot to Javascript
-// Definitely not its best form! But no nice way to do interactive python plotly on web
-
 window.addEventListener('load', function(){
-function unitball3d(p) {
-  // Odd number in order to pick up edges in z-direction
+  
+  // Must have an odd number of steps in order for edges to display correctly
   const smoothness = 6;
   const num_steps = 2 ** smoothness + 1;
 
-  // Initialize theta, phi mesh grid
-  // This looks horrific, but it reduces function calls to Math which makes things very fast
-  // There's probably a much better way to do this in Javascript!!!!
-  var theta = Array(num_steps);
-  var phi   = Array(num_steps);
-  var sintheta = Array(num_steps);
-  var sinphi   = Array(num_steps);
-  var costheta = Array(num_steps);
-  var cosphi   = Array(num_steps);
-  var x = Array(num_steps);
-  var y = Array(num_steps);
-  var z = Array(num_steps);
+  // Generate our ranges of theta & phi values and compute their sines and cosines
+  let theta = [...Array(num_steps).keys()].map(x => (x * Math.PI * 2) / (num_steps - 1) );
+  let phi   = [...Array(num_steps).keys()].map(x => (x * Math.PI)     / (num_steps - 1) );
 
-  // Initialization ctd
-  for (var i = 0; i < num_steps; i++) {
-    theta[i] = Array(num_steps);
-    phi[i]   = Array(num_steps);
-    sintheta[i] = Array(num_steps);
-    sinphi[i]   = Array(num_steps);
-    costheta[i] = Array(num_steps);
-    cosphi[i]   = Array(num_steps);
-    x[i] = Array(num_steps);
-    y[i] = Array(num_steps);
-    z[i] = Array(num_steps);
-  }
+  let sin = {theta: theta.map(x => Math.sin(x)), phi: phi.map(x => Math.sin(x))};
+  let cos = {theta: theta.map(x => Math.cos(x)), phi: phi.map(x => Math.cos(x))};
 
-  // Generate 2d mesh grid of phi and theta-values
-  for (var i = 0; i < num_steps; i++) {
-    var theta_i = (i * Math.PI * 2) / (num_steps - 1);
-    var phi_i   = (i * Math.PI) / (num_steps - 1);
+  // Compute the x, y, z coordinates for a given p-value
+  function unitball3d(p) {
 
-    var sintheta_i = Math.sin(theta_i);
-    var costheta_i = Math.cos(theta_i);
+    let x = [...Array(num_steps)].map( () => Array(num_steps));
+    let y = [...Array(num_steps)].map( () => Array(num_steps));
+    let z = [...Array(num_steps)].map( () => Array(num_steps));
 
-    var sinphi_i = Math.sin(phi_i);
-    var cosphi_i = Math.cos(phi_i);
-
-    for (var j = 0; j < num_steps; j++) {
-      theta[j][i] = theta_i;
-      phi[i][j]   = phi_i;
-
-      sintheta[j][i] = sintheta_i;
-      sinphi[i][j]   = sinphi_i;
-
-      costheta[j][i] = costheta_i;
-      cosphi[i][j]   = cosphi_i;
+    let r = {phi: Array(num_steps), theta: Array(num_steps)};
+  
+    // Calculate our rho values
+    for (let i = 0; i < num_steps; i++) {
+      r.phi[i] = (Math.abs(sin.phi[i])**p + Math.abs(cos.phi[i])**p)**(-1/p);
+      r.theta[i] = (Math.abs(sin.theta[i])**p + Math.abs(cos.theta[i])**p)**(-1/p);
     }
+  
+    // Create a mesh grid of Cartesian points
+    for (let i = 0; i < num_steps; i++) {
+      for (let j = 0; j < num_steps; j++) {
+        x[i][j] = sin.phi[i] * r.phi[i] * cos.theta[j] * r.theta[j];
+        y[i][j] = sin.phi[i] * r.phi[i] * sin.theta[j] * r.theta[j];
+        z[i][j] = cos.phi[i] * r.phi[i];
+      }
+    }
+  
+    return {x, y, z};
   }
 
-  // Helper math function for computation below
-  function rho(varb, i, j) {
-    if (varb == "phi") {
-      var sinx = sinphi[i][j];
-      var cosx = cosphi[i][j];
+  // Initialize the plot with p = 2
+  let p = 2;
+  let layout = {autosize: true, height: 430, margin: {l: 10, r: 10, b: 10, t: 10, pad: 4}};
+  let data_options = {type: 'surface', colorscale: 'Viridis', showscale: false};
+  let data = [{...unitball3d(p), ...data_options}];
+
+  Plotly.newPlot('plot', data, layout, {responsive: true});
+
+  // Update the plot when the user updates the p input field
+  let number = document.getElementById('number');
+  
+  number.addEventListener("change", function(){
+    // Only update the plot if the user-entered p-value is valid
+    if (number.checkValidity()) {
+      p = parseFloat(number.value);
+      data = [{...unitball3d(p), ...data_options}];
+      Plotly.react('plot', data, layout, {responsive: true});
     }
     else {
-      var sinx = sintheta[i][j];
-      var cosx = costheta[i][j];
+      alert("Please choose a p-value between 0.5 and 999.");
     }
-    return (Math.abs(sinx)**p + Math.abs(cosx)**p)**(-1/p)
-  }
-
-  // Generate Cartesian points
-  for (var i = 0; i < num_steps; i++) {
-    for (var j = 0; j < num_steps; j++) {
-      var r_phi   = rho("phi", i, j);
-      var r_theta = rho("theta", i, j);
-
-      x[i][j] = sinphi[i][j] * r_phi * costheta[i][j] * r_theta;
-      y[i][j] = sinphi[i][j] * r_phi * sintheta[i][j] * r_theta;
-      z[i][j] = cosphi[i][j] * r_phi;
-    }
-  }
-
-  return [x, y, z];
-}
-
-// Initialize with p = 2
-var points = unitball3d(2);
-
-// Put the data into the right format for Plotly
-var data = [{
-  x: points[0],
-  y: points[1],
-  z: points[2],
-  type: 'surface',
-  colorscale: 'Viridis',
-  showscale: false,
-}];
-
-var layout = {
-  autosize: true,
-  height: 430,
-  margin: {
-    l: 10,
-    r: 10,
-    b: 10,
-    t: 10,
-    pad: 4
-  }
-};
-
-// Generate the plot
-Plotly.newPlot('plot', data, layout, {responsive: true});
-
-// Tie the plot to the number field input to update plot when user updates p
-var number = document.getElementById('number');
-number.addEventListener("change", function(){
-  // Only update the plot if user-entered number is valid
-  if (number.checkValidity()) {
-    p = parseFloat(number.value);
-    points = unitball3d(p);
-    data = [{
-      x: points[0],
-      y: points[1],
-      z: points[2],
-      type: 'surface',
-      colorscale: 'Viridis',
-      showscale: false,
-    }];
-
-    Plotly.react('plot', data, layout, {responsive: true});
-  }
-  else {
-    alert("Please choose a p-value between 0.5 and 999.");
-  }
-});
+  });
 });
